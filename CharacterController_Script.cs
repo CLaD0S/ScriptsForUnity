6859313174d506e982442d955b6a3e12c1e87a4e
EgoreTTa <kocetaev@gmail.com>
2021-01-20 02:21:47 +0500

    Добавил перемещение

---
+++ CharacterController_Script.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController_Script : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.down);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.left);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().AddForce(Vector2.down);
+        }
+    }
+}
