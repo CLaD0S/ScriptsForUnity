c57bbab847e05edd218aa0ea8ee93c3f402de3af
EgoreTTa <kocetaev@gmail.com>
2021-01-20 19:20:07 +0500

    Добавил поля и инициализацию

--- ItemScript.cs
+++ ItemScript.cs
@@ -6,6 +6,18 @@ using UnityEngine.UI;
 
 public class ItemScript : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField]
+    protected GameObject itemForWorld;
+    [SerializeField]
+    protected string itemName;
+    [SerializeField]
+    protected string description;
+
+    private void Awake()
+    {
+        if (itemName != "") { gameObject.name = itemName; } else { Debug.LogWarning("itemName = null"); }
+        if (description != "") { transform.GetChild(0).GetChild(0).GetComponent<Text>().text = description; } else { Debug.LogWarning("description = null"); }
+    }
     public void OnBeginDrag(PointerEventData eventData)
     {
         transform.GetComponent<Image>().raycastTarget = false;
