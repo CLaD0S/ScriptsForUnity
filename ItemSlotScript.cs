ae4988fbe507168202c24d61c1be56114fdae717
EgoreTTa <kocetaev@gmail.com>
2021-01-21 00:24:51 +0500

    Добавлено перечисление типов предметов, обработка событий.

--- ItemSlotScript.cs
+++ ItemSlotScript.cs
@@ -5,10 +5,20 @@ using UnityEngine.EventSystems;
 
 public class ItemSlotScript : MonoBehaviour, IDropHandler
 {
+    [SerializeField]
+    private ESlotType slotType;
+    private enum ESlotType
+    {
+        all, sword, body, helmet
+    }
     public void OnDrop(PointerEventData eventData)
     {
         if ((eventData.pointerDrag != null) && (eventData.pointerDrag.transform.parent != transform))
         {
+            if (eventData.pointerDrag.GetComponent<ItemScript>().ItemType == ESlotType.all)
+            {
+
+            }
             if (transform.childCount > 0)
             {
                 Transform move = transform.GetChild(0);
