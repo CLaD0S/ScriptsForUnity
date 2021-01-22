25f1bbd480d0e82741266c40f7a4fa019fdb4a4f
EgoreTTa <kocetaev@gmail.com>
2021-01-23 00:40:19 +0500

    Расширен список интерфейсов

--- IInterfaces.cs
+++ IInterfaces.cs
@@ -4,6 +4,26 @@ using UnityEngine;
 
 namespace IInterfaces
 {
+    interface ITiming
+    {
+        void Timing();
+    }
+    interface IHeiling
+    {
+        void Heiling();
+    }
+    interface IDamaging
+    {
+        void Damaging();
+    }
+    interface IBaffDamage
+    {
+        void BaffDamage();
+    }
+    interface IMaximingHitPoint
+    {
+        void MaximingHitPoint();
+    }
     interface IPickUp
     {
         void PicUping(Transform parent);
