682649d989cf486caa25c539f6280cc646fdd1ed
EgoreTTa <kocetaev@gmail.com>
2021-01-23 02:24:25 +0500

    Добавлены функции подсвечивания ячеек взаимодействия.

--- ItemScript.cs
+++ ItemScript.cs
@@ -15,15 +15,18 @@ public class ItemScript : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
     [SerializeField]
     protected string description;
     [SerializeField]
+    protected GameObject InventorySetPanel;
+    [SerializeField]
     public EItemType ItemType;
     public enum EItemType
     {
-        all, sword, body, helmet
+        all, sword, body, helmet, ring, boots, hardword, Konsuella, sopojki, zone
     }
     protected void Awake()
     {
         if (itemName != "") { gameObject.name = itemName; } else { Debug.LogWarning("itemName = null"); }
         if (description != "") { transform.GetChild(0).GetChild(0).GetComponent<Text>().text = description; } else { Debug.LogWarning("description = null"); }
+        InventorySetPanel = GameObject.Find("InventorySetPanel");
     }
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -32,11 +35,35 @@ public class ItemScript : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDr
     public void OnDrag(PointerEventData eventData)
     {
         transform.GetComponent<RectTransform>().anchoredPosition += eventData.delta;
+        for (int i = 0; i <= InventorySetPanel.transform.childCount - 1; i++)
+        {
+            if (InventorySetPanel.transform.GetChild(i).GetComponent<ItemSlotScript>() != null)
+            {
+                if (Equals(InventorySetPanel.transform.GetChild(i).GetComponent<ItemSlotScript>().SlotType.ToString(), ItemType.ToString()))
+                {
+                    if (InventorySetPanel.transform.GetChild(i) != transform.parent)
+                    {
+                        InventorySetPanel.transform.GetChild(i).GetComponent<Image>().color = new Color(0, 1, 0);
+                    }
+                }
+            }
+        }
     }
+
     public void OnEndDrag(PointerEventData eventData)
     {
         transform.GetComponent<Image>().raycastTarget = true;
         transform.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
+        for (int i = 0; i <= InventorySetPanel.transform.childCount - 1; i++)
+        {
+            if (InventorySetPanel.transform.GetChild(i).GetComponent<ItemSlotScript>() != null)
+            {
+                if (Equals(InventorySetPanel.transform.GetChild(i).GetComponent<ItemSlotScript>().SlotType.ToString(), ItemType.ToString()))
+                {
+                    InventorySetPanel.transform.GetChild(i).GetComponent<Image>().color = new Color(1, 1, 1);
+                }
+            }
+        }
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
