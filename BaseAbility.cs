8b0d974bedc334b86e5db685c3df16e4bd90e31c
EgoreTTa <kocetaev@gmail.com>
2021-01-27 03:14:28 +0500

    Проверка работы композиции эффектов

---
+++ BaseAbility.cs
@@ -0,0 +1,15 @@
+using Effects;
+using UnityEngine;
+
+namespace Abilities
+{
+    public abstract class BaseAbility : MonoBehaviour
+    {
+
+    }
+
+    public class Heal : BaseAbility
+    {
+
+    }
+}
