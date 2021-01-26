1e1668e96b7278f3f98dc2dc01d78902329386f1
EgoreTTa <kocetaev@gmail.com>
2021-01-27 03:12:17 +0500

    Проверка работы композиции эффектов

--- TestingScriptForAbility.cs
+++ TestingScriptForAbility.cs
@@ -2,12 +2,23 @@ using Effects;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestingScriptForAbility : MonoBehaviour
 {
-    public Healing heal;
+    public Image panelStatus;
+    public GameObject panels;
+
+    public List<BaseEffect> effects;
+    public List<Sprite> images;
     private void Awake()
     {
-        heal = gameObject.AddComponent<Healing>();
+        effects.Add(gameObject.AddComponent<Healing>());
+        effects.Add(gameObject.AddComponent<Damaging>());
+        for (int i = 0; i < images.Count; i++)
+        {
+            GameObject slots = Instantiate(panels, panelStatus.transform);
+            slots.GetComponent<Image>().sprite = images[i];
+        }
     }
 }
