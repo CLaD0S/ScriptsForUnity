829355d0bc3021f0b31e1649e9cdf78a43d25f2b
EgoreTTa <kocetaev@gmail.com>
2021-01-28 03:03:14 +0500

    Настроено взаимодействие и передача эффекта другому Игровому Объекту

---
+++ Behavior_item_script.cs
@@ -0,0 +1,24 @@
+using Effects;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behavior_item_script : MonoBehaviour
+{
+    private BaseEffect effect;
+    private void Awake()
+    {
+        effect = gameObject.AddComponent<Healing>();
+        effect.enabled = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Character")
+        {
+            GetComponent<CircleCollider2D>().enabled = false;
+            transform.parent.GetComponent<SpriteRenderer>().color = new Color(0.2f, 0.2f, 0.2f, 0.2f);
+            effect.gameObject.transform.SetParent(collision.transform);
+            effect.enabled = true;
+        }
+    }
+}
