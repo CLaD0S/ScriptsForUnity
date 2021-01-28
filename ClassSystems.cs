88d806e340d8b8e62904c91a12eae86a8556d5f9
EgoreTTa <kocetaev@gmail.com>
2021-01-28 18:27:14 +0500

    Расписана система здоровья

--- ClassSystems.cs
+++ ClassSystems.cs
@@ -1,18 +1,71 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ClassSystems : MonoBehaviour
+namespace ClassSystems
 {
-    // Start is called before the first frame update
-    void Start()
+    public class HealSystem : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private float heal;
+        [SerializeField] private float maxHeal;
+        [SerializeField] private float regenHeal;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        float dtime = 0;
+        public GameObject panel;
+        public float Heal
+        {
+            get => heal;
+            set
+            {
+                heal = value;
+                ChangeHeal();
+            }
+        }
+        public float MaxHeal
+        {
+            get => maxHeal;
+            set
+            {
+                maxHeal = value;
+                ChangeMaxHeal();
+            }
+        }
+        public float RegenHeal
+        {
+            get => regenHeal;
+            set
+            {
+                regenHeal = value;
+                ChangeRegenHeal();
+            }
+        }
+        public HealSystem(float heal = 100f, float maxHeal = 100f, float regenHeal = 1f)
+        {
+            this.heal = heal;
+            this.maxHeal = maxHeal;
+            this.regenHeal = regenHeal;
+        }
+        private void ChangeHeal()
+        {
+            panel.GetComponent<Slider>().value = heal;
+            if (heal < maxHeal) Regeneration();
+        }
+        private void ChangeMaxHeal()
+        {
+            panel.GetComponent<Slider>().maxValue = maxHeal;
+        }
+        private void ChangeRegenHeal()
+        {
+
+        }
+        private IEnumerator Regeneration()
+        {
+            do
+            {
+                yield return new WaitForSeconds(1 / 5);
+                heal += regenHeal;
+            } while (heal < maxHeal);
+            heal = maxHeal;
+        }
     }
 }
