e6b5ca762b703e585d61ee47c9ee797e0022b208
EgoreTTa <kocetaev@gmail.com>
2021-01-30 02:33:37 +0500

    Произведен рефакторинг

--- ClassSystems.cs
+++ ClassSystems.cs
@@ -5,100 +5,105 @@ using UnityEngine.UI;
 
 namespace ClassSystems
 {
-    public class HealSystem
+    public class PointsSystem
     {
-        [SerializeField] private float heal;
-        [SerializeField] private float maxHeal;
-        [SerializeField] private float regenHeal;
+        [SerializeField] private float points;
+        [SerializeField] private float maxPoints;
+        [SerializeField] private float regenPoints;
+        [SerializeField] private float delay = 2.0f;
+        [SerializeField] private float periodicity = .2f;
+        [SerializeField] private bool lastChangePositiv = true;
         private GameObject panel;
         private MonoBehaviour mono;
         private IEnumerator coroutineRegeneration;
-        public float Heal
+        public float Points
         {
-            get => heal;
+            get => points;
             set
             {
                 if (value > 0)
                 {
-                    heal = value;
-                    ChangeHeal();
+                    lastChangePositiv = value >= points;
+                    points = value;
+                    ChangePoints();
                 }
                 else
                 {
-                    heal = 0;
+                    points = 0;
                     UpdateState();
                     Debug.Log(mono.name + " погиб");
                 }
             }
         }
-        public float MaxHeal
+        public float MaxPoints
         {
-            get => maxHeal;
+            get => maxPoints;
             set
             {
-                maxHeal = value;
-                ChangeMaxHeal();
+                maxPoints = value;
+                ChangeMaxPoints();
             }
         }
-        public float RegenHeal
+        public float RegenPoints
         {
-            get => regenHeal;
+            get => regenPoints;
             set
             {
-                regenHeal = value;
-                ChangeRegenHeal();
+                regenPoints = value;
+                ChangeRegenPoints();
             }
         }
-        public HealSystem(MonoBehaviour mono, float heal = 50f, float maxHeal = 100f, float regenHeal = 1f)
+        public PointsSystem(MonoBehaviour mono, float points = 50f, float maxPoints = 100f, float regenPoints = 1f)
         {
-            this.heal = heal;
-            this.maxHeal = maxHeal;
-            this.regenHeal = regenHeal;
+            this.points = points;
+            this.maxPoints = maxPoints;
+            this.regenPoints = regenPoints;
             panel = GameObject.Find("/Canvas/Slider");
             this.mono = mono;
             this.coroutineRegeneration = Regeneration();
             mono.StartCoroutine(coroutineRegeneration);
         }
-        private void ChangeHeal()
+        private void ChangePoints()
         {
             UpdateState();
-            if ((heal < maxHeal) && (!coroutineRegeneration.MoveNext()))
+            if ((points < maxPoints) && (!coroutineRegeneration.MoveNext()))
             {
                 mono.StartCoroutine(coroutineRegeneration);
             }
         }
-        private void ChangeMaxHeal()
+        private void ChangeMaxPoints()
         {
             UpdateState();
         }
-        private void ChangeRegenHeal()
+        private void ChangeRegenPoints()
         {
 
         }
         private void UpdateState()
         {
-            panel.GetComponent<Slider>().value = heal;
-            panel.GetComponent<Slider>().maxValue = maxHeal;
-            panel.GetComponentInChildren<Text>().text = "Здоровье :" + heal.ToString("#.##") + "/" + maxHeal.ToString("#.##");
+            panel.GetComponent<Slider>().value = points;
+            panel.GetComponent<Slider>().maxValue = maxPoints;
+            panel.GetComponentInChildren<Text>().text = "Здоровье :" + points.ToString("#.##") + "/" + maxPoints.ToString("#.##");
         }
         private IEnumerator Regeneration()
         {
             while (true)
             {
+                if (!lastChangePositiv) yield return new WaitForSeconds(delay);
                 do
                 {
-                    yield return new WaitForSeconds(1f / 5f);
-                    heal += regenHeal / 5f;
+                    yield return new WaitForSeconds(periodicity);
+                    points += regenPoints;
                     UpdateState();
-                } while (heal < maxHeal);
-                heal = maxHeal;
+                } while (points < maxPoints);
+                points = maxPoints;
                 UpdateState();
                 mono.StopCoroutine(coroutineRegeneration);
             }
         }
         public override string ToString()
         {
-            return "Тек. хп=" + heal + "\nТек. маx.хп=" + maxHeal + "\nТек. регенер=" + regenHeal;
+            return "Тек. хп=" + points + "\nТек. маx.хп=" + maxPoints + "\nТек. регенер=" + regenPoints;
         }
     }
 }
